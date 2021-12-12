1086ccb3591da5c3de72bc9b6f5b0596663ba5c1
successJohn <87600912+successJohn@users.noreply.github.com>
2021-12-12 19:38:27 +0100

    feat: Added Validation for models

---
+++ middlewares/authValidation.js
@@ -0,0 +1,17 @@
+const {check} = require("express-validator");
+
+
+exports.registerValidation = [
+    check("email", "Please enter a valid email").isEmail(),
+    check("firstName", "firstName is required").not().isEmpty(),
+    check("lastName", "lastName is required").not().isEmpty(),
+    check("password", "please enter an alphanumeric password with 6 or more characters").isAlphanumeric().isLength({min:6}),
+    check("confirmPassword", "Passwords do not match").custom(
+        (value, {req}) =>  value === req.body.password
+    )
+];
+
+exports.loginValidation = [
+    check('email', "please enter a valid email").isEmail(),
+    check("password","please enter an alphanumeric password with 6 or more characters").isAlphanumeric().isLength({min:6})
+]
